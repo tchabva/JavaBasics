6cd2da2a4501df3baea177c31aa6e86aa8398b62
Tawanda Chabva <tchabva@gmail.com>
2024-10-16 14:41:59 +0100

    feat: added Library class to git repo

---
+++ src/main/java/org/example/books/Library.java
@@ -0,0 +1,14 @@
+package org.example.books;
+
+import java.util.ArrayList;
+
+public class Library {
+    ArrayList<Ebook> ebookArrayList = new ArrayList<>();
+    ArrayList<GraphicNovel> graphicNovelArrayList = new ArrayList<>();
+    ArrayList<SheetMusic> sheetMusicArrayList = new ArrayList<>();
+
+    //Create a deposit book to Library method.
+
+    //Create a withdraw book from Library method.
+}
+
