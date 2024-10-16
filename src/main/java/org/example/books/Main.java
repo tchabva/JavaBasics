770a538ccce92d86eda8c54ad8ecfb76ea7c3ffc
Tawanda Chabva <tchabva@gmail.com>
2024-10-16 15:23:03 +0100

    feat: tested the features of Libray in the Main Class

--- src/main/java/org/example/books/Main.java
+++ src/main/java/org/example/books/Main.java
@@ -13,12 +13,28 @@ public class Main {
         System.out.println(book.title);
 
         Ebook ebook = new Ebook("The Hobbit", 800, FileType.EPUB);
+        Ebook ebook1 = new Ebook("The Revolution", 474, FileType.PDF);
 
         System.out.println(ebook.pages);
         System.out.println(ebook.title);
         System.out.println(ebook.fileType);
 
+        //Creating an instance of library
+        Library library = new Library();
 
+        library.depositBook(ebook);
+
+        System.out.println(library.getEbookArrayList().size());
+
+        library.depositBook(ebook1);
+
+        System.out.println(library.getEbookArrayList().size());
+        System.out.println(library.getEbookArrayList().getFirst().title);
+
+        library.withdrawBook(ebook);
+
+        System.out.println(library.getEbookArrayList().size());
+        System.out.println(library.getEbookArrayList().getFirst().title);
     }
 }
 
